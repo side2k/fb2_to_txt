37a13bbed6404086177bf384111f50e470f46f08
Leonid Amirov <leonid.amirov@gmail.com>
2013-05-01 19:41:31 +0400

    bugfix: text output for multiple nested tags

--- fb2_to_txt.py
+++ fb2_to_txt.py
@@ -8,10 +8,10 @@ from lxml import etree
 file_path = sys.argv[1]
 tree = etree.parse(file_path)
 root = tree.getroot()
-for element in root.xpath("//*"):
+for element in root:
     tag = element.xpath("local-name()")
     if tag == "binary":
         continue
-    text = (element.text if element.text else '') + element.tail if element.tail else ''
-    if text:
-        print text.encode("utf-8")
+    for text in element.itertext():
+        if text.strip():
+            print text.encode("utf-8")
